3fa3c2757a9e0e3ae1cd4c03cb8282b2fae41e93
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-02-03 16:40:28 +0100

    Add comics cards con prop

---
+++ src/components/ComicsProp.vue
@@ -0,0 +1,29 @@
+<script>
+    export default {
+        name: "ComicsProp",
+        props: {
+            src: String,
+            title: String
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <img :src="src">
+
+        <h5>{{ title }}</h5>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    img {
+        width: 100%;
+    }
+
+    h5 {
+        color: white;
+        width: 100%;
+        margin-top: 0.5rem;
+    }
+</style>
